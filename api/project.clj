a772ce81fd7fd6f38a58f6425ed02379ae8bdb9b
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-05-15 16:28:15 +0200

    Add base project

---
+++ api/project.clj
@@ -0,0 +1,10 @@
+(defproject api "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [clojurewerkz/elastisch "3.0.0-beta2"]]
+  :main ^:skip-aot api.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ api/src/api/core.clj
@@ -0,0 +1,11 @@
+(ns api.core
+  (:require [clojurewerkz.elastisch.rest :as esr])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+
+(def conn nil)
