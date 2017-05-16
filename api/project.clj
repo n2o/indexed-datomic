57d7de747f59188d0b1ff4e0e9d137daf0d6e1ba
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-05-16 10:13:11 +0200

    Add dependency; add API to docker-compose

--- api/project.clj
+++ api/project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [clojurewerkz/elastisch "3.0.0-beta2"]]
+                 [clojurewerkz/elastisch "3.0.0-beta2"]
+                 [org.clojure/core.async "0.3.442"]
+                 [com.datomic/clj-client "0.8.606"]]
   :main ^:skip-aot api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
